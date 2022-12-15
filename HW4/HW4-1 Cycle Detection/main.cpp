dfb076ebc441e0d8494ef0bb610c31c409d5af6b
jovi <jovi920204@gmail.com>
2022-12-15 16:08:22 +0800

    HW4-1 AC

--- HW4/HW4-1 Cycle Detection/main.cpp
+++ HW4/HW4-1 Cycle Detection/main.cpp
@@ -3,46 +3,67 @@
 #include <string>
 using namespace std;
 
+class Queue {
+private:
+    vector<int> data;
+
+public:
+    int front() {
+        return this->data[0];
+    }
+    void push(int x) {
+        data.push_back(x);
+    };
+    void pop() {
+        vector<int> temp = data;
+        data.clear();
+        data.assign(temp.begin() + 1, temp.end());
+    };
+    bool empty() {
+        return data.empty();
+    };
+};
+
 bool checkCycle(int sourceNode);
 
+
+vector<vector<int> > graph;
+int nodeNumber;
+
 int main(){
-    int nodeNumber;
     cin >> nodeNumber;
-    vector<vector<int> > graph(nodeNumber);
+    graph.resize(nodeNumber, vector<int>());
     int startNode, endNode;
     while(cin >> startNode >> endNode){
         graph[startNode].push_back(endNode);
     }
-
-    for (int i=0;i<nodeNumber;i++){
-        cout << i << " : ";
-        for (int j=0;j<graph[i].size();j++){
-            cout << graph[i][j] << " ";
-        }
-        cout << endl;
-    }
-
     for (int i=0;i<nodeNumber;i++){
         if (checkCycle(i)){
             cout << "Cycle";
-            break;
+            return 0;
         }
     }
     cout << "No Cycle";
-    
-
     return 0;
 }
 
 bool checkCycle(int sourceNode){
-    // TODO
+    vector<bool> used(nodeNumber,false);
+    used[sourceNode] = true;
+    Queue q;
+    q.push(sourceNode);
+    while(!q.empty()){
+        int curr = q.front();
+        q.pop();
+        for (int i=0;i<graph[curr].size();i++){
+            if (graph[curr][i] == sourceNode){
+                return 1;
+            }
+            if (used[graph[curr][i]] == false){
+                used[graph[curr][i]] = true;
+                q.push(graph[curr][i]);
+            }
+        }
+    }
+    return 0;
 }
-
-/*
-
-3
-0 1
-1 2
-2 0
-
-*/
